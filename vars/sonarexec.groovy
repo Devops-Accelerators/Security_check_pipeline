c8490275106e76532ffc6584362208b24a125c18
DevOps-Accelerators <sasdevops@gmail.com>
2019-06-12 15:03:37 +0530

    Update sonarexec.groovy

--- vars/sonarexec.groovy
+++ vars/sonarexec.groovy
@@ -4,7 +4,15 @@ def call(String s) {
     {
            sh "echo ${s}" 
         sh """echo ${Sonarscanner}"""
-     sh  """${Sonarscanner}/sonar-scanner -Dsonar.host.url=${s} -Dsonar.login=admin -Dsonar.password=soumianisoumya@123"""
-        
+     sh  """${Sonarscanner}/sonar-scanner -Dsonar.host.url=${s} -Dsonar.login=admin -Dsonar.password=soumianisoumya@123"""   
+    }
+    
+    timeout (time: 1, unit: 'HOURS')
+    {
+       def qg = waitForQualityGate()
+       if (qg.status != 'OK')
+       {
+              error "Pipeline aborted due to quality gate failure: ${qg.status}"
+       }
     }
         }
